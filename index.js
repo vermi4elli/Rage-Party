06b55b3b7173a605b9fda4635fcbf746dc55723b
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-10-08 16:28:31 +0300

    Replaced Koa with Fastify

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 'use strict';
 
-const Koa = require('koa');
-const app = new Koa();
-const Router = require('koa-router');
-const router = new Router();
+const fastify = require('fastify')({ logger: true });
+const path = require('path');
 
-router.get('/', async ctx => {
-  ctx.body = 'Hello, world!';
-  console.log('Succeed!');
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname),
+  default: '/'
+  //prefix: '/public/', // optional:
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+fastify.get('/another/path', (req, reply) => {
+  // serving path.join(__dirname, 'public', 'myHtml.html') directly
+  reply.sendFile('index.html');
+});
 
-app.listen(1234);
+// fastify.get('/path/with/different/root', (req, reply) => {
+//   // serving a file from a different root location
+//   reply.sendFile('myHtml.html', path.join(__dirname, 'build'));
+// });
+
+
+// Declare a route
+//fastify.get('/', async (request, reply) => ({ hello: 'world' }));
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
