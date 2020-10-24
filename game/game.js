77dbc0a4ee0928e156516ccfbddb336028a2215c
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-10-25 01:43:34 +0300

    Merged game js-s into 1

--- game/game.js
+++ game/game.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// import * as PIXI from 'pixi.js';
 // import { keyboard } from './keyboard';
 
 const renderer =
@@ -117,6 +116,46 @@ function shoot(mX, mY) {
   bullet.scale.y = 0.2;
 
   //ScreenShake(10);
+}
+
+const collisionType = {
+  no: 0,
+  top: 1,
+  right: 2,
+  down: 3,
+  left: 4
+};
+
+function contain(sprite, container) {
+
+  const collision = {
+    x: collisionType.no,
+    y: collisionType.no
+  };
+
+  //Left
+  if (container.x >= sprite.x - 90) {
+    collision.x = collisionType.left;
+  }
+
+  // Right
+  if (sprite.position.x + sprite.width + 90 >=
+    container.position.x + container.width) {
+    collision.x = collisionType.right;
+  }
+
+  //Top
+  if (sprite.position.y - 90 + (sprite.height * 0.8) <= container.position.y) {
+    collision.y = collisionType.top;
+  }
+
+  // Down
+  if (sprite.position.y + sprite.height + 90 >=
+    container.position.y + container.height) {
+    collision.y = collisionType.down;
+  }
+
+  return collision;
 }
 
 // character control
