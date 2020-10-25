9ef5a1bc449afcd8b7c47968ae7a71e4f30a97d3
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-10-26 00:18:55 +0200

    Added the crosshair style

--- game/game.js
+++ game/game.js
@@ -15,6 +15,10 @@ const stage = new PIXI.Container();
 const bunnyTexture = PIXI.Texture.fromImage('assets/bunny.png');
 const bulletTexture = PIXI.Texture.fromImage('assets/bullet.png');
 const mapTexture = PIXI.Texture.fromImage('assets/levels/baseMap.png');
+const defaultIcon = 'url(\'./assets/utils/crosshair.cur\'),auto';
+
+// Add custom cursor styles
+renderer.plugins.interaction.cursorStyles.default = defaultIcon;
 
 // create a background
 const dungeon = new PIXI.Sprite(mapTexture);
